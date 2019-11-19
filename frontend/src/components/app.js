7bd7f1d683bb30049457fde524f0aea1b082a3be
ninja <rngyowjr@gmail.com>
2019-11-19 13:28:06 -0800

    Before I start breaking stuff

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom'
+import '../stylesheets/app.scss';
 
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import '../../stylesheets/login.scss';
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -60,7 +61,7 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="login-content-container">
         <form onSubmit={this.handleSubmit}>
           <div>
             <input
