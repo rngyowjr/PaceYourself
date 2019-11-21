6146e41b3d98f40ab2392b59b5f783d2b8f35386
ninja <rngyowjr@gmail.com>
2019-11-21 12:50:45 -0800

    fixed entities user store on logout

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,17 +1,23 @@
-import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
-import {
-  RECEIVE_USER,
-} from "../actions/users_actions";
+import { RECEIVE_CURRENT_USER, RECEIVE_USER_LOGOUT } from "../actions/session_actions";
+// import {
+//   RECEIVE_USER,
+// } from "../actions/users_actions";
 
-const usersReducer = (state = {}, action) => {
+const initialState = {
+    user: {}
+};
+
+const usersReducer = (state = initialState, action) => {
   Object.freeze(state);
   let nextState;
 
   switch (action.type) {
-    case RECEIVE_USER:
+    // case RECEIVE_USER:
     case RECEIVE_CURRENT_USER:
       nextState = Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
       return nextState;
+    case RECEIVE_USER_LOGOUT:
+        return initialState;
     default:
       return state;
   }
