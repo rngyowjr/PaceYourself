79c8220aafdeec2335236c1584ce5d758c775f4a
ninja <rngyowjr@gmail.com>
2019-11-21 12:25:58 -0800

    sign up has undefined auth token

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,19 +1,19 @@
-import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
-import {
-  RECEIVE_USER,
-} from "../actions/users_actions";
+// import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+// import {
+//   RECEIVE_USER,
+// } from "../actions/users_actions";
 
-const usersReducer = (state = {}, action) => {
-  Object.freeze(state);
-  let nextState;
+// const usersReducer = (state = {}, action) => {
+//   Object.freeze(state);
+//   let nextState;
 
-  switch (action.type) {
-    case RECEIVE_USER:
-    case RECEIVE_CURRENT_USER:
-      nextState = Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
-      return nextState;
-    default:
-      return state;
-  }
-};
-export default usersReducer;
+//   switch (action.type) {
+//     case RECEIVE_USER:
+//     case RECEIVE_CURRENT_USER:
+//       nextState = Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+//       return nextState;
+//     default:
+//       return state;
+//   }
+// };
+// export default usersReducer;
