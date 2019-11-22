7af781313895c525d86587dce24436854ce3ec14
ninja <rngyowjr@gmail.com>
2019-11-22 15:32:26 -0800

    fixed footer

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Footer = () => {
-    return (
-        <footer className="footer-container">
-            <div className="footer">
-                <nav className="footer-nav">
-                    <ul className="footer-nav-list">
-                        <li>About</li>
-                        <li>Jobs </li>
-                        <li>Blog </li>
-                        <li>Developers </li>
-                        <li>Guidelines </li>
-                        <li>Privacy </li>
-                        <li>Terms </li>
-                        <li>Help </li>
-                        <li>Help forum </li>
-                        <li>English </li>
-                    </ul>
-                </nav>
-            </div>
-        </footer>
-    )
+class Footer extends React.Component {
+
+    render () {
+        return (
+            <footer className="footer-container">
+                <div className="footer">
+                    <nav className="footer-nav">
+                        <ul className="footer-nav-list">
+                            <Link>About</Link>
+                            <Link>Developers </Link>
+                            <a
+                                className="footer-aa-link"
+                                href="https://www.appacademy.io/"
+                            >
+                            </a>
+                        </ul>
+                    </nav>
+                </div>
+            </footer>
+        )
+    }
 };
 
 export default Footer;
