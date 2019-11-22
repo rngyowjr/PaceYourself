1589eb30a6cf07dc810431964808c8696f5af3c2
ninja <rngyowjr@gmail.com>
2019-11-22 12:31:16 -0800

    before creating footer container

--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -96,6 +96,24 @@ class LoginForm extends React.Component {
             > Sign Up Here  </Link>
           </div>
         </form>
+        <footer className="footer-container">
+          <div className="footer">
+            <nav className="footer-nav">
+              <ul className="footer-nav-list">
+                <li>About</li>
+                <li>Jobs </li>
+                <li>Blog </li>
+                <li>Developers </li>
+                <li>Guidelines </li>
+                <li>Privacy </li>
+                <li>Terms </li>
+                <li>Help </li>
+                <li>Help forum </li>
+                <li>English </li>
+              </ul>
+            </nav>
+          </div>
+        </footer>
       </div>
     );
   }
