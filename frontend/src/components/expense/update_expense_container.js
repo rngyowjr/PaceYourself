957e6c82a57d985df59618683c0baa98891a90c3
Julie Tran <kimtran767@gmail.com>
2019-12-04 16:22:17 -0800

    added update income container

--- frontend/src/components/expense/update_expense_container.js
+++ frontend/src/components/expense/update_expense_container.js
@@ -1,18 +1,8 @@
 import { connect } from 'react-redux';
-import { updateExpense, fetchExpense, fetchAllExpenses } from '../../actions/expense_actions';
+import { updateExpense, fetchExpense } from '../../actions/expense_actions';
 import UpdateExpenseForm from './update_expense_form';
 
 const mstp = (state, ownProps) => {
-    debugger
-    // let expense;
-
-    // if (state.entities.expenses.data !== undefined)  {
-    //     expense = Object.values(state.entities.expenses.data).filter(data => 
-    //         data._id === ownProps.match.params.expenseId
-    //     ) 
-    // } else {
-    //     expense = {};
-    // };
     return ({
         currentUser: state.session.user,
         // expense: expense,
@@ -22,7 +12,6 @@ const mstp = (state, ownProps) => {
 };
 
 const mdtp = dispatch => ({
-    fetchAllExpenses: () => dispatch(fetchAllExpenses()),
     fetchExpense: expenseId => dispatch(fetchExpense(expenseId)),
     action: expense => dispatch(updateExpense(expense))
 });
--- frontend/src/components/income/update_income.jsx
+++ frontend/src/components/income/update_income.jsx
@@ -1,66 +1,66 @@
-// import React from 'react';
-// import '../../stylesheets/income.scss'
-// import Navbar from '../nav/navbar_container';
+import React from 'react';
+import '../../stylesheets/income.scss'
+import Navbar from '../nav/navbar_container';
 
-// class UpdateIncome extends React.Component {
-//   constructor(props){
-//     super(props)
+class UpdateIncome extends React.Component {
+  constructor(props){
+    super(props)
 
-//     this.handleUpdate = this.handleUpdate.bind(this);
-//   }
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
 
-//   componentDidMount() {
-//     this.props.fetchAllIncome();
-//   }
+  componentDidMount() {
+    this.props.fetchAllIncome();
+  }
 
-//   handleUpdate(incomeId) {
+  handleUpdate(incomeId) {
 
-//   }
+  }
 
-//   render() {
+  render() {
     
-//     return (
-//       <div className="main-div">
-//         <Navbar />
-//         <form onSubmit={this.handleSubmit}>
-//           <label>Month:
-//             <select onChange={this.update("month")} defaultValue="select">
-//               <option value="select" disabled="disabled">Select Month</option>
-//               <option value="January">January</option>
-//               <option value="February">February</option>
-//               <option value="March">March</option>
-//               <option value="April">April</option>
-//               <option value="May">May</option>
-//               <option value="June">June</option>
-//               <option value="July">July</option>
-//               <option value="August">August</option>
-//               <option value="September">September</option>
-//               <option value="October">October</option>
-//               <option value="November">November</option>
-//               <option value="December">December</option>
-//             </select>
-//           </label>
-//           <br />
-//           <label>Year:
-//             <input
-//               type="number"
-//               min="2019" max="2025"
-//               placeholder="YYYY"
-//               onChange={this.update("year")}
-//             />
-//           </label>
-//           <br />
-//           <label>Monthly Income: $
-//             <input
-//               type="number"
-//               min="1"
-//               onChange={this.update("income")}
-//               step="0.01"
-//             />
-//           </label>
-//           <button>Submit</button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
+    return (
+      <div className="main-div">
+        <Navbar />
+        <form onSubmit={this.handleSubmit}>
+          <label>Month:
+            <select onChange={this.update("month")} defaultValue="select">
+              <option value="select" disabled="disabled">Select Month</option>
+              <option value="January">January</option>
+              <option value="February">February</option>
+              <option value="March">March</option>
+              <option value="April">April</option>
+              <option value="May">May</option>
+              <option value="June">June</option>
+              <option value="July">July</option>
+              <option value="August">August</option>
+              <option value="September">September</option>
+              <option value="October">October</option>
+              <option value="November">November</option>
+              <option value="December">December</option>
+            </select>
+          </label>
+          <br />
+          <label>Year:
+            <input
+              type="number"
+              min="2019" max="2025"
+              placeholder="YYYY"
+              onChange={this.update("year")}
+            />
+          </label>
+          <br />
+          <label>Monthly Income: $
+            <input
+              type="number"
+              min="1"
+              onChange={this.update("income")}
+              step="0.01"
+            />
+          </label>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
