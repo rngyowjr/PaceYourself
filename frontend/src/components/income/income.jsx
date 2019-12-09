c664c47b6a673982b3d52031ba620c0abbf3497a
ninja <rngyowjr@gmail.com>
2019-12-09 15:35:15 -0500

    fixed errors

--- frontend/src/components/income/income.jsx
+++ frontend/src/components/income/income.jsx
@@ -10,7 +10,7 @@ class Income extends React.Component {
       income: 0,
     }
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.closeIncome = this.closeIncome.bind(this);
+    this.closeIncomeForm = this.closeIncomeForm.bind(this);
   }
 
   update(field) {
@@ -74,58 +74,56 @@ class Income extends React.Component {
       <div className="income-content">
         <form className="income-form" onSubmit={this.handleSubmit}>
           <label>Month:
-                <select onChange={this.update("month")} defaultValue="select">
-                  <option value="select" disabled="disabled">Select Month</option>    
-                  <option value="January">January</option>
-                  <option value="February">February</option>
-                  <option value="March">March</option>
-                  <option value="April">April</option>
-                  <option value="May">May</option>
-                  <option value="June">June</option>
-                  <option value="July">July</option>
-                  <option value="August">August</option>
-                  <option value="September">September</option>
-                  <option value="October">October</option>
-                  <option value="November">November</option>
-                  <option value="December">December</option>
-                </select>
-              </label>
-              <br />
-              <label>Year:
-                  <input 
-                    className="income-year-input"
-                    type="number" 
-                    min="2019" max="2025" 
-                    placeholder="YYYY"
-                    onChange={this.update("year")}
-                  />
-              </label>
-              <br />
-              <label>Monthly Income: $
+              <select onChange={this.update("month")} defaultValue="select">
+                <option value="select" disabled="disabled">Select Month</option>    
+                <option value="January">January</option>
+                <option value="February">February</option>
+                <option value="March">March</option>
+                <option value="April">April</option>
+                <option value="May">May</option>
+                <option value="June">June</option>
+                <option value="July">July</option>
+                <option value="August">August</option>
+                <option value="September">September</option>
+                <option value="October">October</option>
+                <option value="November">November</option>
+                <option value="December">December</option>
+              </select>
+            </label>
+            <br />
+            <label>Year:
                 <input 
-                    className="income-value-input"
-                    type="number" 
-                    min="1" 
-                    onChange={this.update("income")}
-                    step="0.01" 
+                  className="income-year-input"
+                  type="number" 
+                  min="2019" max="2025" 
+                  placeholder="YYYY"
+                  onChange={this.update("year")}
                 />
-              </label>
-              <br/>
-              <div className="income-button-container">
-                <button 
-                  type="button" 
-                  onClick={this.closeIncomeForm} 
-                  className="income-cancel-button"
-                >Cancel</button>
-                <button className="income-submit-button">Submit</button>
-              </div>
-              {/* <button 
-                className="income-flip-button"
-                type="button"
-                onClick={this.flipIncomeForm}
-              >FLIP!</button> */}
-            </form>
-          </div>
+            </label>
+            <br />
+            <label>Monthly Income: $
+              <input 
+                  className="income-value-input"
+                  type="number" 
+                  min="1" 
+                  onChange={this.update("income")}
+                  step="0.01" 
+              />
+            </label>
+            <br/>
+            <div className="income-button-container">
+              <button 
+                type="button" 
+                onClick={this.closeIncomeForm} 
+                className="income-cancel-button"
+              >Cancel</button>
+              <button className="income-submit-button">Submit</button>
+            </div>
+            {/* <button 
+              className="income-flip-button"
+              type="button"
+              onClick={this.flipIncomeForm}
+            >FLIP!</button> */}
         </form>
       </div>
     );
